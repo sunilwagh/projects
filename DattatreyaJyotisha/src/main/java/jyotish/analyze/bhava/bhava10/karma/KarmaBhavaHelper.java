3bbbd86b70e8cc52a26e46b40f50fda16b95d3a7
Sunil Wagh <sunilwagh2004@gmail.com>
2017-09-09 22:55:08 -0400

    Modified Karma Bhava

--- DattatreyaJyotisha/src/main/java/jyotish/analyze/bhava/bhava10/karma/KarmaBhavaHelper.java
+++ DattatreyaJyotisha/src/main/java/jyotish/analyze/bhava/bhava10/karma/KarmaBhavaHelper.java
@@ -18,56 +18,59 @@ public class KarmaBhavaHelper {
 
 	public static String infoAboutLordOfBhava10InBhava1(Bhava10 bhava10,Bhava1 bhava1) {
 		StringBuffer infoBuffer = new StringBuffer();
-		infoBuffer.append("");
+		infoBuffer.append("You fulfill your ambitions without help of anybody. Career is priority. At times there is confucion between personal "
+				+ "needs and worldly outer goals.");
 		return infoBuffer.toString();
 		
 	}
 
 	public static String infoAboutLordOfBhava10InBhava2(Bhava10 bhava10,Bhava2 bhava2) {
 		StringBuffer infoBuffer = new StringBuffer();
-		infoBuffer.append("");
+		infoBuffer.append("Ability to become wealthy through family connections and inheritance.");
 		return infoBuffer.toString();
 		
 	}
 
 	public static String infoAboutLordOfBhava10InBhava3(Bhava10 bhava10,Bhava3 bhava3) {
 		StringBuffer infoBuffer = new StringBuffer();
-		infoBuffer.append("");
+		infoBuffer.append("You will achieve your ambitions through your own efforts, a career in communications is possible.");
 		return infoBuffer.toString();
 		
 	}
 
 	public static String infoAboutLordOfBhava10InBhava4(Bhava10 bhava10,Bhava4 bhava4) {
 		StringBuffer infoBuffer = new StringBuffer();
-		infoBuffer.append("");
+		infoBuffer.append("Outer ambitions and inner aspirations are in harmony.");
 		return infoBuffer.toString();
 		
 	}
 
 	public static String infoAboutLordOfBhava10InBhava5(Bhava10 bhava10,Bhava5 bhava5) {
 		StringBuffer infoBuffer = new StringBuffer();
-		infoBuffer.append("");
+		infoBuffer.append("Enahances the creative connections of your career and gives you tremendous insights.");
 		return infoBuffer.toString();
 		
 	}
 
 	public static String infoAboutLordOfBhava10InBhava6(Bhava10 bhava10,Bhava6 bhava6) {
 		StringBuffer infoBuffer = new StringBuffer();
-		infoBuffer.append("");
+		infoBuffer.append("Success through hardwork. Both six and ten are Upachaya houses and so they will improve with time.");
 		return infoBuffer.toString();
 		
 	}
 
 	public static String infoAboutLordOfBhava10InBhava7(Bhava10 bhava10,Bhava7 bhava7) {
 		StringBuffer infoBuffer = new StringBuffer();
-		infoBuffer.append("");
+		infoBuffer.append("Your career is connected with partnership issues. You need others to work with you or you can make"
+				+ " a good career where relationships are involved like marriage counsellor, therapists..");
 		return infoBuffer.toString();
 		
 	}
 	
 	public static String infoAboutLordOfBhava10InBhava8(Bhava10 bhava10,Bhava8 bhava8) {
 		StringBuffer infoBuffer = new StringBuffer();
-		infoBuffer.append("Career in research,astrology,history or issues around reincarnation. It can also show scandals related to career.");
+		infoBuffer.append("Career in research,astrology,history or issues around reincarnation. It can also show "
+				+ "scandals related to career.");
 		return infoBuffer.toString();
 		
 	}
@@ -75,14 +78,15 @@ public class KarmaBhavaHelper {
 
 	public static String infoAboutLordOfBhava10InBhava9(Bhava10 bhava10,Bhava9 bhava9) {
 		StringBuffer infoBuffer = new StringBuffer();
-		infoBuffer.append("");
+		infoBuffer.append("In the 9th house, this indicates luck. It creates a combination for success "
+				+ "through knowledge,teaching and higher learning.");
 		return infoBuffer.toString();
 		
 	}
 	
 	public static String infoAboutLordOfBhava10InBhava10(Bhava10 bhava10) {
 		StringBuffer infoBuffer = new StringBuffer();
-		infoBuffer.append("");
+		infoBuffer.append("Prominent personality. Career,ambitions and sense of individuality are strong.");
 		return infoBuffer.toString();
 		
 		
@@ -90,21 +94,23 @@ public class KarmaBhavaHelper {
 	
 	public static String infoAboutLordOfBhava10InBhava11(Bhava10 bhava10,Bhava11 bhava11) {
 		StringBuffer infoBuffer = new StringBuffer();
-		infoBuffer.append("");
+		infoBuffer.append("In the 11th house which is a house of gains it indicates the ability to realize your "
+				+ "ambitions and make money.");
 		return infoBuffer.toString();
 		
 	}
 	
 	public static String infoAboutLordOfBhava10InBhava12(Bhava10 bhava10,Bhava12 bhava12) {
 		StringBuffer infoBuffer = new StringBuffer();
-		infoBuffer.append("");
+		infoBuffer.append("In the 12th house, it shows success away from home. There is also connection with institutions"
+				+ ",hospitals and those who work for the benefit of others.");
 		return infoBuffer.toString();
 		
 	}
 	
 	public static String AriesInBhava10() {
 		StringBuffer ariesInBhava1 = new StringBuffer();
-		ariesInBhava1.append(" \n");
+		ariesInBhava1.append("\n");
 		ariesInBhava1.append(" \n");
 		return ariesInBhava1.toString();
 	}
@@ -153,36 +159,36 @@ public class KarmaBhavaHelper {
 
 	public static String ScorpioInBhava10() {
 		StringBuffer taurusInBhava1 = new StringBuffer();
-		taurusInBhava1.append("Ascendent: Scorpio \n");
-		taurusInBhava1.append("Maraka: Mangal \n");
+		taurusInBhava1.append("\n");
+		taurusInBhava1.append("\n");
 		return taurusInBhava1.toString();
 	}
 
 	public static String SagittariusInBhava10() {
 		StringBuffer taurusInBhava1 = new StringBuffer();
-		taurusInBhava1.append("Ascendent: Sagittarius \n");
-		taurusInBhava1.append("Maraka: Mangal \n");
+		taurusInBhava1.append("\n");
+		taurusInBhava1.append("\n");
 		return taurusInBhava1.toString();
 	}
 
 	public static String CapricornInBhava10() {
 		StringBuffer taurusInBhava1 = new StringBuffer();
-		taurusInBhava1.append("Ascendent: Capricorn \n");
-		taurusInBhava1.append("Maraka: Mangal \n");
+		taurusInBhava1.append("\n");
+		taurusInBhava1.append("\n");
 		return taurusInBhava1.toString();
 	}
 
 	public static String AcquariusInBhava10() {
 		StringBuffer taurusInBhava1 = new StringBuffer();
-		taurusInBhava1.append("Ascendent: Acquarius \n");
-		taurusInBhava1.append("Maraka: Mangal \n");
+		taurusInBhava1.append("\n");
+		taurusInBhava1.append("\n");
 		return taurusInBhava1.toString();
 	}
 
 	public static String PiscesInBhava10() {
 		StringBuffer taurusInBhava1 = new StringBuffer();
 		taurusInBhava1.append("Ascendent: Pisces \n");
-		taurusInBhava1.append("Maraka: Mangal \n");
+		taurusInBhava1.append("\n");
 		return taurusInBhava1.toString();
 	}
 
