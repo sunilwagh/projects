a6a31880ba2f2b7b1a7b27a24e350cb7d79f677b
Sunil Wagh <sunilwagh2004@gmail.com>
2017-09-11 23:37:09 -0400

    Nore Changes

--- DattatreyaJyotisha/src/main/java/jyotish/grahas/Shani.java
+++ DattatreyaJyotisha/src/main/java/jyotish/grahas/Shani.java
@@ -7,6 +7,7 @@ import jyotish.bhavas.Bhava;
 import jyotish.nakshatras.Nakshatra;
 import jyotish.nakshatras.padas.Pada;
 import jyotish.rashis.Rashi;
+import util.JyotishEnumUtil.Guna;
 import util.JyotishEnumUtil.Name_Of_Bhava;
 import util.JyotishEnumUtil.Name_Of_Graha;
 import util.JyotishEnumUtil.Name_Of_Nakshatra;
@@ -15,87 +16,114 @@ import util.JyotishEnumUtil.Tattva;
 
 public class Shani implements Graha{
 	
+	private static final Set<Name_Of_Rashi> LORDOFRASHIS = new HashSet<Name_Of_Rashi>();
+	private static final Name_Of_Graha GRAHA_NAME = Name_Of_Graha.SHANI;
+	private static final Name_Of_Rashi MOOLTRIKONARASHI = Name_Of_Rashi.ARIES;
+	private static final Name_Of_Rashi EXALTATION = Name_Of_Rashi.VIRGO;
+	private static final Name_Of_Rashi DEBILITATION = Name_Of_Rashi.PISCES;
+	private static final Name_Of_Rashi OWN = Name_Of_Rashi.GEMINI;
+	private static final Tattva TATTVA = Tattva.VAYU;
+	private static final Set<Name_Of_Nakshatra> LORDOFNAKSHATRA = new HashSet<Name_Of_Nakshatra>();
+	private static final Set<Name_Of_Bhava> KARAKA = new HashSet<Name_Of_Bhava>();
+	private static final Set<Name_Of_Graha> ENEMIES_TO_GRAHA = new HashSet<Name_Of_Graha>();
+	private static final Set<Name_Of_Graha> FRIENDS_TO_GRAHA = new HashSet<Name_Of_Graha>();
+	private static final Set<Name_Of_Graha> NUETRAL_TO_GRAHA = new HashSet<Name_Of_Graha>();
+	private static final Guna GUNA = Guna.TAMASIC;
+	private static final Set<Name_Of_Bhava> STRONGEST = new HashSet<Name_Of_Bhava>();
+	private static final Set<Name_Of_Bhava> STRONG = new HashSet<Name_Of_Bhava>();
+	private static final Set<Name_Of_Bhava> NUETRAL = new HashSet<Name_Of_Bhava>();
+	private static final Set<Name_Of_Bhava> WEAK = new HashSet<Name_Of_Bhava>();
+	private static final Set<Name_Of_Bhava> WEAKEST = new HashSet<Name_Of_Bhava>();
+
+	static {
+		addFriendsToGraha();
+		addEnemiesToGraha();
+		addLordOfNakshatrasToGraha();
+		addKarakasToGraha();
+		addLordOfRashisToGraha();
+		addBhavasToStrongest();
+		addBhavasToStrong();
+		addBhavasToNuetral();
+		addBhavasToWeak();
+		addBhavasToWeakest();
+
+	}
+	
+	
+	public static void addLordOfNakshatrasToGraha() {
+		LORDOFNAKSHATRA.add(Name_Of_Nakshatra.PUSHYA);
+		LORDOFNAKSHATRA.add(Name_Of_Nakshatra.ANURADHA);
+		LORDOFNAKSHATRA.add(Name_Of_Nakshatra.UTTAR_BHADRAPADA);
+	}
+
+	private static void addLordOfRashisToGraha() {
+		LORDOFRASHIS.add(Name_Of_Rashi.CAPRICORN);
+		LORDOFRASHIS.add(Name_Of_Rashi.ACQUARIUS);
+
+	}
+
+	public static void addKarakasToGraha() {
+		KARAKA.add(Name_Of_Bhava.BHAVA4);
+		KARAKA.add(Name_Of_Bhava.BHAVA10);
+
+	}
+
+	public static void addEnemiesToGraha() {
+		ENEMIES_TO_GRAHA.add(Name_Of_Graha.CHANDRA);
+
+	}
+
+	public static void addFriendsToGraha() {
+		FRIENDS_TO_GRAHA.add(Name_Of_Graha.SURYA);
+		FRIENDS_TO_GRAHA.add(Name_Of_Graha.SHUKRA);
+
+	}
+
+	public static void addNuetralToGraha() {
+		NUETRAL_TO_GRAHA.add(Name_Of_Graha.MANGAL);
+		NUETRAL_TO_GRAHA.add(Name_Of_Graha.GURU);
+		NUETRAL_TO_GRAHA.add(Name_Of_Graha.SHANI);
+
+	}
+
+	public static void addBhavasToStrongest() {
+		STRONGEST.add(Name_Of_Bhava.BHAVA7);
+	}
+
+	public static void addBhavasToStrong() {
+		STRONG.add(Name_Of_Bhava.BHAVA3);
+		STRONG.add(Name_Of_Bhava.BHAVA6);
+		STRONG.add(Name_Of_Bhava.BHAVA10);
+		STRONG.add(Name_Of_Bhava.BHAVA11);
+		
+
+	}
+
+	public static void addBhavasToNuetral() {
+		
+
+	}
+
+	public static void addBhavasToWeak() {
+		WEAK.add(Name_Of_Bhava.BHAVA2);
+		WEAK.add(Name_Of_Bhava.BHAVA4);
+		WEAK.add(Name_Of_Bhava.BHAVA5);
+		WEAK.add(Name_Of_Bhava.BHAVA9);
+		WEAK.add(Name_Of_Bhava.BHAVA12);
+
+	}
+
+	public static void addBhavasToWeakest() {
+		WEAKEST.add(Name_Of_Bhava.BHAVA1);
+
+	}
+
 	Rashi lagnaKundaliRashi;
 	Nakshatra nakshatra;
 	Bhava bhava;
 	Pada pada;
 	double degrees;
 	
-	private final static Name_Of_Graha grahaName = Name_Of_Graha.SHANI;
-	final static Name_Of_Rashi moolTrikonaRashi=Name_Of_Rashi.CANCER;
-	private final static Name_Of_Rashi EXALTATION=Name_Of_Rashi.VIRGO;
-	private final static Name_Of_Rashi DEBILITATION=Name_Of_Rashi.PISCES;
-	private final static Name_Of_Rashi OWN=Name_Of_Rashi.GEMINI;
-	private final static Tattva tattva = Tattva.PRITHVI;
-	private final static Set<Name_Of_Nakshatra> LORDOFNAKSHATRA = new HashSet<Name_Of_Nakshatra>();
-	private final static Set<Name_Of_Bhava> KARAKA = new HashSet<Name_Of_Bhava>();
-	private final static Set<Name_Of_Graha> ENEMIES_TO_GRAHA = new HashSet<Name_Of_Graha>();
-	private final static Set<Name_Of_Graha> FRIENDS_TO_GRAHA = new HashSet<Name_Of_Graha>();
-	private final static Set<Name_Of_Graha> NUETRAL_TO_GRAHA = new HashSet<Name_Of_Graha>();
-	
-	public Rashi getLagnaKundaliRashi() {
-		return lagnaKundaliRashi;
-	}
-	public void setLagnaKundaliRashi(Rashi lagnaKundaliRashi) {
-		this.lagnaKundaliRashi = lagnaKundaliRashi;
-	}
-	public Nakshatra getNakshatra() {
-		return nakshatra;
-	}
-	public void setNakshatra(Nakshatra nakshatra) {
-		this.nakshatra = nakshatra;
-	}
-	public Bhava getBhava() {
-		return bhava;
-	}
-	public void setBhava(Bhava bhava) {
-		this.bhava = bhava;
-	}
-	public Pada getPada() {
-		return pada;
-	}
-	public void setPada(Pada pada) {
-		this.pada = pada;
-	}
-	public static Name_Of_Graha getGrahaname() {
-		return grahaName;
-	}
-	public static Name_Of_Rashi getMooltrikonarashi() {
-		return moolTrikonaRashi;
-	}
-	public static Name_Of_Rashi getExaltation() {
-		return EXALTATION;
-	}
-	public static Name_Of_Rashi getDebilitation() {
-		return DEBILITATION;
-	}
-	public static Name_Of_Rashi getOwn() {
-		return OWN;
-	}
-	public static Tattva getTattva() {
-		return tattva;
-	}
-	public static Set<Name_Of_Nakshatra> getLordofnakshatra() {
-		return LORDOFNAKSHATRA;
-	}
-	public static Set<Name_Of_Bhava> getKaraka() {
-		return KARAKA;
-	}
-	public static Set<Name_Of_Graha> getEnemiesToGraha() {
-		return ENEMIES_TO_GRAHA;
-	}
-	public static Set<Name_Of_Graha> getFriendsToGraha() {
-		return FRIENDS_TO_GRAHA;
-	}
-	public static Set<Name_Of_Graha> getNuetralToGraha() {
-		return NUETRAL_TO_GRAHA;
-	}
-	public double getDegrees() {
-		return degrees;
-	}
-	public void setDegrees(double degrees) {
-		this.degrees = degrees;
-	}
 	
 	
 	
