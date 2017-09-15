4a9a61fdaaa6e67a43feccde6d44c41c9a7783b5
Sunil Wagh <sunilwagh2004@gmail.com>
2017-09-15 15:32:19 -0400

    Charging.

--- DattatreyaJyotisha/src/main/java/jyotish/analyze/bhavas/bhava1/lagna/ascendant/CancerAscendant.java
+++ DattatreyaJyotisha/src/main/java/jyotish/analyze/bhavas/bhava1/lagna/ascendant/CancerAscendant.java
@@ -1,5 +1,11 @@
 package jyotish.analyze.bhavas.bhava1.lagna.ascendant;
 
 public class CancerAscendant {
+	
+	 public enum FUNCTIONAL_BENEFIC{MANGAL,CHANDRA};
+	
+	 public enum FUNCTIONAL_MALEFIC{};
+		
+	 public enum FUNCTIONAL_NUETRAL{};
 
 }
