db2e20a1071f2ee57ba20da7a30fcf13d9b34c96
Sunil Wagh <sunilwagh2004@gmail.com>
2017-09-15 19:37:47 -0400

    Making changes.

--- DattatreyaJyotisha/src/main/java/jyotish/nakshatras/Vishaka.java
+++ DattatreyaJyotisha/src/main/java/jyotish/nakshatras/Vishaka.java
@@ -1,5 +1,29 @@
 package jyotish.nakshatras;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Vishaka implements Nakshatra{
+	
+	private static List<String> features = new ArrayList<String>();
+	
+	static {
+		features.add("Manifestation");			
+		features.add("New Oppurtunities");				
+		features.add("Courageous");				
+		features.add("Indiscriminate");				
+		features.add("Sexual Gratification");				
+		features.add("Highly Individualistic and Self Reliant");				
+		features.add("Aspire for higher position and authority in life");				
+		features.add("Ability to overcome obstacles");				
+		features.add("Unforgiving or Indulgences or");				
+		features.add("Determination and Persistance");				
+		features.add("Enthusiastic");				
+		features.add("The diety is Indragni, who are binary. Indra is zero and Agni is one. There is internal conflict between opposing desires.");				
+		features.add("Always in state of readiness. Always focussed like Arjuna focussed on the eye on the fish.");
+	
+	
+
+	}
 
 }
