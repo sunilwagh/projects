ab59e70c35b2730110246d0a813496b52b699c91
Sunil Wagh <sunilwagh2004@gmail.com>
2017-09-15 19:18:26 -0400

    Making changes.

--- DattatreyaJyotisha/src/main/java/jyotish/nakshatras/Chitra.java
+++ DattatreyaJyotisha/src/main/java/jyotish/nakshatras/Chitra.java
@@ -1,5 +1,26 @@
 package jyotish.nakshatras;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Chitra implements Nakshatra{
+	
+private static List<String> features = new ArrayList<String>();
+	
+static {
+	features.add("Beauty");	
+	features.add("Highly Creative");
+	features.add("Good Deeds");	
+	features.add("Brilliant");	
+	features.add("Creativity during gloom of life");	
+	features.add("Ambigiuos or Unbalanced");
+	features.add("Sudden flashes of insight");
+	features.add("Hardworking");	
+	features.add("Problem with child and family rivalry");
+	features.add("Vindictive or Manipulative");
+	features.add("They are very good at queuing up. They are very disciplined at dinner table.");	
+	features.add("They are very good at ambush marketing and guerilla warfare- taking the enemy by surprise");
+	features.add("They may be good mimics");
+	}
 
 }
--- DattatreyaJyotisha/src/main/java/jyotish/nakshatras/Swati.java
+++ DattatreyaJyotisha/src/main/java/jyotish/nakshatras/Swati.java
@@ -1,5 +1,27 @@
 package jyotish.nakshatras;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Swati implements Nakshatra{
+	
+	private static List<String> features = new ArrayList<String>();
+	
+	static {
+		features.add("Transformation");	
+		features.add("Individualistic or Independent");
+		features.add("Dextorous and Resourceful");	
+		features.add("Travel and Speed");	
+		features.add("Hardworking");	
+		features.add("Freedom Loving");	
+		features.add("Powerful");	
+		features.add("Dynamic Achiever");	
+		features.add("Unforgiving or Indulgences or");	
+		features.add("Impulsive");	
+		features.add("Fearless");	
+		features.add("They love spending a long time in the bathtub");	
+		features.add("Very good with multiple musical instruments");	
+		features.add("Could have a medicinal herb garden");	
+	}
 
 }
