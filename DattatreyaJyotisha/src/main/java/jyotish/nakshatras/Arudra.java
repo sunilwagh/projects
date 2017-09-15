57a153335548ab53548eebea29a233bb89389b02
Sunil Wagh <sunilwagh2004@gmail.com>
2017-09-15 18:13:47 -0400

    Making changes.

--- DattatreyaJyotisha/src/main/java/jyotish/nakshatras/Arudra.java
+++ DattatreyaJyotisha/src/main/java/jyotish/nakshatras/Arudra.java
@@ -1,5 +1,28 @@
 package jyotish.nakshatras;
 
-public class Arudra implements Nakshatra{
+import java.util.ArrayList;
+import java.util.List;
+
+public class Arudra implements Nakshatra {
+
+	private static List<String> features = new ArrayList<String>();
+
+	static {
+
+		features.add("Fierce and Sharp");
+		features.add("Achiever");
+		features.add("Addictions");
+		features.add("Researcher or Analytical");
+		features.add("Sacrifices");
+		features.add("Rash or Impulsive");
+		features.add("Emotional");
+		features.add("Easily pleased and easily angered");
+		features.add("Ability to withstand pressure");
+		features.add("Hardworking");
+		features.add("Highly prone to mood swings");
+		features.add(
+				"Women with prominent Ardra may show symptoms of preganancy even when not pregnant. (Psuedosysis)");
+		features.add("Patience is their forte, specially Ardra 4th are highly reputed and wealthy.");
+	}
 
 }
