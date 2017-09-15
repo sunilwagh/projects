877a510f20d410a7ec71548acc9e3d2dc7fb10f7
Sunil Wagh <sunilwagh2004@gmail.com>
2017-09-15 16:08:36 -0400

    Changing

---
+++ batch/src/main/java/com/dattatreya/batch/step/SimpleStep.java
@@ -0,0 +1,23 @@
+package com.dattatreya.batch.step;
+
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+ 
+public class SimpleStep implements Tasklet{
+ 
+	@Override
+	public RepeatStatus execute(StepContribution contribution,
+			ChunkContext chunkContext) throws Exception {
+		workload();
+		System.out.println("Done");
+		return RepeatStatus.FINISHED;
+	}
+	
+	private void workload() throws Exception{
+		Thread.sleep(5000);
+	}
+ 
+}
+
