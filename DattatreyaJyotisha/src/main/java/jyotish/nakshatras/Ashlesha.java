44eff15c9141e02a23ab0b6b4d6393d8ee8206ac
Sunil Wagh <sunilwagh2004@gmail.com>
2017-09-15 18:28:01 -0400

    Making changes.

--- DattatreyaJyotisha/src/main/java/jyotish/nakshatras/Ashlesha.java
+++ DattatreyaJyotisha/src/main/java/jyotish/nakshatras/Ashlesha.java
@@ -1,5 +1,28 @@
 package jyotish.nakshatras;
 
-public class Ashlesha implements Nakshatra{
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ashlesha implements Nakshatra {
+
+	private static List<String> features = new ArrayList<String>();
+
+	static {
+
+		features.add("Clinging");
+		features.add("Dangerous or Devious");
+		features.add("Deceptive");
+		features.add("Intuitive or Perception or Wisdom");
+		features.add("Concentration");
+		features.add("Sexual");
+		features.add("Instinctive");
+		features.add("Genetic Issue or Genetic Heritage");
+		features.add("Calculative");
+		features.add("Spiritual Awakening");
+		features.add("They consume or should consume Spirulina");
+		features.add("They could be very keen on activating their Kundalini");
+		features.add(
+				"They are willing to go any extent for their own folks. 1st part is good for politics and social work.");
+	}
 
 }
