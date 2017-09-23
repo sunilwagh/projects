0d960ba94bb3db99f7b0b7427d03a1a62e769917
Sunil Wagh <sunilwagh2004@gmail.com>
2017-09-23 16:48:30 -0400

    Making changes.

--- DattatreyaJyotisha/src/main/java/jyotish/nakshatras/Chitra.java
+++ DattatreyaJyotisha/src/main/java/jyotish/nakshatras/Chitra.java
@@ -3,24 +3,27 @@ package jyotish.nakshatras;
 import java.util.ArrayList;
 import java.util.List;
 
-public final class Chitra implements Nakshatra{
-	
-private static List<String> features = new ArrayList<String>();
-	
-static {
-	features.add("Beauty");	
-	features.add("Highly Creative");
-	features.add("Good Deeds");	
-	features.add("Brilliant");	
-	features.add("Creativity during gloom of life");	
-	features.add("Ambigiuos or Unbalanced");
-	features.add("Sudden flashes of insight");
-	features.add("Hardworking");	
-	features.add("Problem with child and family rivalry");
-	features.add("Vindictive or Manipulative");
-	features.add("They are very good at queuing up. They are very disciplined at dinner table.");	
-	features.add("They are very good at ambush marketing and guerilla warfare- taking the enemy by surprise");
-	features.add("They may be good mimics");
+import util.NakshatraEnumUtil.NAKSHTRA_TYPE;
+
+public final class Chitra implements Nakshatra {
+
+	private final NAKSHTRA_TYPE NAKSHATRA_TYPE = NAKSHTRA_TYPE.MRUDU;
+	private static List<String> features = new ArrayList<String>();
+
+	static {
+		features.add("Beauty");
+		features.add("Highly Creative");
+		features.add("Good Deeds");
+		features.add("Brilliant");
+		features.add("Creativity during gloom of life");
+		features.add("Ambigiuos or Unbalanced");
+		features.add("Sudden flashes of insight");
+		features.add("Hardworking");
+		features.add("Problem with child and family rivalry");
+		features.add("Vindictive or Manipulative");
+		features.add("They are very good at queuing up. They are very disciplined at dinner table.");
+		features.add("They are very good at ambush marketing and guerilla warfare- taking the enemy by surprise");
+		features.add("They may be good mimics");
 	}
 
 }
