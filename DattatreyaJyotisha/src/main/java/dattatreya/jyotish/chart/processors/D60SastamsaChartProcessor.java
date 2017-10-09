5d1bc3e5a1368d77f1bc2df03eeb9db3d19a7549
Sunil Wagh <sunilwagh2004@gmail.com>
2017-10-09 00:25:39 -0400

    Making changes.

---
+++ DattatreyaJyotisha/src/main/java/dattatreya/jyotish/chart/processors/D60SastamsaChartProcessor.java
@@ -0,0 +1,27 @@
+package dattatreya.jyotish.chart.processors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import dattatreya.jyotish.analyze.bhavas.Chart;
+import dattatreya.jyotish.analyze.bhavas.ChartProcessor;
+import dattatreya.jyotish.horoscope.D9NavamsaChart;
+
+@Component("d60SastamsaChartProcessor")
+@Order(value=16)
+public class D60SastamsaChartProcessor implements ChartProcessor{
+	
+	@Autowired
+	@Qualifier("d60SastamsaChart") 
+	Chart horoscope;
+
+	public Chart getHoroscope() {
+		return horoscope;
+	}
+
+	public void setHoroscope(Chart horoscope) {
+		this.horoscope = horoscope;
+	}
+}
