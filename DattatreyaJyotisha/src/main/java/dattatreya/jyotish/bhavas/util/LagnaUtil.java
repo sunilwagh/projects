f6452ac06efb774bd43c0d896c2c74bcd4ecdd8c
Sunil Wagh <sunilwagh2004@gmail.com>
2017-10-16 00:01:58 -0400

    Making changes.

---
+++ DattatreyaJyotisha/src/main/java/dattatreya/jyotish/bhavas/util/LagnaUtil.java
@@ -0,0 +1,78 @@
+package dattatreya.jyotish.bhavas.util;
+
+import dattatreya.jyotish.analyze.bhavas.Chart;
+import dattatreya.jyotish.rashis.RashiUtil;
+import dattatreya.jyotish.util.RashiNames;
+
+public class LagnaUtil {
+	
+	public static boolean isLagnaAries(Chart chart)
+	{
+		return isLagna(chart,RashiNames.ARIES);
+	}
+	
+	public static boolean isLagnaTaurus(Chart chart)
+	{
+		return isLagna(chart,RashiNames.TAURUS);
+	}
+	
+	public boolean isLagnaGemini(Chart chart)
+	{
+		return isLagna(chart,RashiNames.GEMINI);
+	}
+	
+	public boolean isLagnaCancer(Chart chart)
+	{
+		return isLagna(chart,RashiNames.CANCER);
+	}
+	
+	public boolean isLagnaLeo(Chart chart)
+	{
+		return isLagna(chart,RashiNames.LEO);
+	}
+	
+	public boolean isLagnaVirgo(Chart chart)
+	{
+		return isLagna(chart,RashiNames.VIRGO);
+	}
+	
+	public boolean isLagnaLibra(Chart chart)
+	{
+		return isLagna(chart,RashiNames.LIBRA);
+	}
+	
+	public boolean isLagnaScorpio(Chart chart)
+	{
+		return isLagna(chart,RashiNames.SCORPIO);
+	}
+	
+	public static boolean isLagnaSagittarius(Chart chart)
+	{
+		return isLagna(chart,RashiNames.SAGITTAURIUS);
+	}
+	
+	public static boolean isLagnaCapricorn(Chart chart)
+	{
+		return isLagna(chart,RashiNames.CAPRICORN);
+	}
+	
+	public static  boolean isLagnaAquarius(Chart chart)
+	{
+		return isLagna(chart,RashiNames.ACQUARIUS);
+	}
+	
+	public static boolean isLagnaPisces(Chart chart)
+	{
+		return isLagna(chart,RashiNames.PISCES);
+	}
+
+	private static boolean isLagna(Chart chart, RashiNames rashiName) {
+		
+		if (RashiUtil.findRashiInBhava(chart.getBhavas()[0].getRashi()) == rashiName){return true;}
+		else
+		{
+		return false;
+		}
+	}
+
+}
