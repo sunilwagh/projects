72166ecad167bcd32b2ecec6fbb3c4e855b2e796
Sunil Wagh <sunilwagh2004@gmail.com>
2017-10-15 22:24:05 -0400

    Making changes.

--- DattatreyaJyotisha/src/main/java/dattatreya/jyotish/bhavas/util/BhavasUtil.java
+++ DattatreyaJyotisha/src/main/java/dattatreya/jyotish/bhavas/util/BhavasUtil.java
@@ -25,4 +25,74 @@ public class BhavasUtil {
 		
 		return chart.getBhavas()[resultBhavaPosition];
 	}
+	
+	public Bhava nextBhava(Bhava bhava, Chart chart, int atPosition)
+	{
+		return getBhava(bhava,chart,1);
+	}
+	
+	public Bhava prevBhava(Bhava bhava, Chart chart, int atPosition)
+	{
+		return getBhava(bhava,chart,-1);
+	}
+	
+	
+	
+	public Bhava secondBhavaFrom(Bhava bhava, Chart chart)
+	{
+		return getBhava(bhava,chart,2);
+	}
+	
+	public Bhava thirdBhavaFrom(Bhava bhava, Chart chart)
+	{
+		return getBhava(bhava,chart,3);
+	}
+	
+	public Bhava fourthBhavaFrom(Bhava bhava, Chart chart)
+	{
+		return getBhava(bhava,chart,4);
+	}
+	
+	public Bhava fifthBhavaFrom(Bhava bhava, Chart chart)
+	{
+		return getBhava(bhava,chart,5);
+	}
+	
+	public Bhava sixthBhavaFrom(Bhava bhava, Chart chart)
+	{
+		return getBhava(bhava,chart,6);
+	}
+	
+	public Bhava seventhBhavaFrom(Bhava bhava, Chart chart)
+	{
+		return getBhava(bhava,chart,7);
+	}
+	
+	public Bhava eightBhavaFrom(Bhava bhava, Chart chart)
+	{
+		return getBhava(bhava,chart,8);
+	}
+	
+	public Bhava ninthBhavaFrom(Bhava bhava, Chart chart)
+	{
+		return getBhava(bhava,chart,9);
+	}
+	
+	public Bhava tenthBhavaFrom(Bhava bhava, Chart chart)
+	{
+		return getBhava(bhava,chart,10);
+	}
+	
+	public Bhava eleventhBhavaFrom(Bhava bhava, Chart chart)
+	{
+		return getBhava(bhava,chart,11);
+	}
+	
+	public Bhava twelvethBhavaFrom(Bhava bhava, Chart chart)
+	{
+		return getBhava(bhava,chart,12);
+	}
+	
+	
+	
 }
