c873985d93ea6118f4a2904183e55648630487df
Sunil Wagh <sunilwagh2004@gmail.com>
2017-10-15 20:11:08 -0400

    Making changes.

--- DattatreyaJyotisha/src/main/java/dattatreya/jyotish/bhavas/grahas/mangal/MangalInBhava12.java
+++ DattatreyaJyotisha/src/main/java/dattatreya/jyotish/bhavas/grahas/mangal/MangalInBhava12.java
@@ -10,7 +10,7 @@ public final class MangalInBhava12 implements MangalInBhava {
 	public String getInfoAboutMangalInBhava() {
 		StringBuilder sb = new StringBuilder();
 		sb.append("\n Mangal in 12th House: \n");
-		sb.append("\n \n");
+		sb.append("\n Brothers early death or relation with brother will be spolied. \n");
 		return sb.toString();
 	}
 
--- DattatreyaJyotisha/src/main/java/dattatreya/jyotish/bhavas/grahas/shukra/ShukraInBhava12.java
+++ DattatreyaJyotisha/src/main/java/dattatreya/jyotish/bhavas/grahas/shukra/ShukraInBhava12.java
@@ -10,7 +10,7 @@ public final class ShukraInBhava12 implements ShukraInBhava {
 	public String getInfoAboutShukraInBhava() {
 		StringBuilder sb = new StringBuilder();
 		sb.append("\n Shukra in 12th House: \n");
-		sb.append("");
+		sb.append("\n Sudden Gains \n");
 		return sb.toString();
 	}
 
